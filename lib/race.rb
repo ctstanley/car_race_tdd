a741333def2094418ab621fa17678bf9a631ba8a
ctstanley <ctstanley1@hotmail.com>
2015-06-03 23:50:54 -0700

    done

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,17 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+	attr_accessor :cars
+	# Initialize with two cars
+	def initialize
+		@cars = Array.new(2) { |i| Car.new }
+		@cars.each { |car| car.accelerate(rand(1..100)) }
+	end
+	def winner
+		cars.first.speed > cars.last.speed ? cars.first : cars.last
+	end
+	def loser
+		cars.first.speed < cars.last.speed ? cars.first : cars.last
+	end
 end
+
